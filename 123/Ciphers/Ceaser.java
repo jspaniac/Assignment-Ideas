d7f54465eac9e36317ec071f2539bb5246753836
jspaniac <jspaniac@cs.washington.edu>
2023-11-25 04:21:10 -0800

    updating to remove setter

--- 123/Ciphers/Ceaser.java
+++ 123/Ciphers/Ceaser.java
@@ -1,13 +1,6 @@
 public class Ceaser extends Cipher {
-    public Ceaser() {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
 
     public Ceaser(String shifter) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
-    public void setShifter(String shifter) {
         throw new UnsupportedOperationException("Not implemented yet!");
     }
 
