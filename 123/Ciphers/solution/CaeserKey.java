3be6dfade3fd5c8cdd8ebaa7dc3261b904ddf07a
Joe Spaniac <36169310+jspaniac@users.noreply.github.com>
2023-12-30 14:36:04 -0800

    Add files via upload

---
+++ 123/Ciphers/solution/CaeserKey.java
@@ -0,0 +1,21 @@
+public class CaeserKey extends Caeser {
+    public CaeserKey(String key) {
+        if (key.equals("")) {
+            throw new IllegalArgumentException("Provided key (\"\") provides no encryption");
+        }
+        super.checkValid(key);
+
+        String shifter = key;
+        for (int i = Cipher.MIN_CHAR; i <= Cipher.MAX_CHAR; i++) {
+            if (shifter.indexOf((char)(i)) == -1) {
+                shifter += (char)(i);
+            }
+        }
+
+        super.setShifter(shifter);
+    }
+
+    public void setShifter(String shifter) {
+        throw new UnsupportedOperationException("Unable to change shifter for CeaserKey");
+    }
+}
---
+++ 123/Ciphers/solution/Concealment.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Concealment extends Cipher {
+    public static final Random RAND = new Random();
+    
+    private int position;
+
+    public Concealment(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("Position must not be negative");
+        }
+        if (position == 0) {
+            throw new IllegalArgumentException("Position for concealment (0) provides no encryption");
+        }
+        this.position = position;
+    }
+
+    public String handleInput(String input, boolean encode) {
+        String ret = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (encode) {
+                for (int j = 0; j < position; j++) {
+                    ret += (char)(Cipher.MIN_CHAR + RAND.nextInt(Cipher.TOTAL_CHARS + 1));
+                }
+            } else {
+                i += position;
+            }
+
+            if (i < input.length()) {
+                ret += input.charAt(i);
+            }
+        }
+        return ret;
+    }
+}
